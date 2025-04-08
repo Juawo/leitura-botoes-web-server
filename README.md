eeac831426f4276c3e7df892ea3321b44c4067b4
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2025-04-08 11:51:21 -0300

    Adicionando README com task's para fazer

---
+++ README.md
@@ -0,0 +1,9 @@
+## Tarefas 
+
+    - [ ] Ler botões A e B
+    - [ ] Enviar estados dos botões a cada 1s para o servidor
+
+### Desafios
+    - [ ] Exibir o estado dos botões em uma página HTML com CSS
+    - [ ] Adicionar sensor de temperatura e enviar informações do mesmo
+    - [ ] Exibir informaçẽos do sensor de temperatura 
--- leitura-botoes-web-server.c
+++ leitura-botoes-web-server.c
@@ -2,13 +2,26 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+#define BTN_A 05
+#define BTN_B 06
 
 
+void setup()
+{
+    gpio_init(BTN_A);
+    gpio_set_dir(BTN_A, GPIO_IN);
+    gpio_pull_up(BTN_A);
+
+    gpio_init(BTN_B);
+    gpio_set_dir(BTN_B, GPIO_IN);
+    gpio_pull_up(BTN_B);
+}
 
 int main()
 {
     stdio_init_all();
-
+    setup();
+    
     // Initialise the Wi-Fi chip
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed\n");
